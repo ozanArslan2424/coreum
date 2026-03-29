52a133e47ba830f157b3d55259ad1b00ac5fd5dd
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2026-03-29 04:11:42 +0300

    docs build fix

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { GlobalRouterStore } from "@/store/GlobalRouterStore";
 import * as C from "@/C";
 import * as X from "@/X";
 
-export const $prefixStore = new GlobalPrefixStore();
-export const $routerStore = new GlobalRouterStore();
-
 export { BranchAdapter } from "@/Router/adapters/BranchAdapter";
 export { MemoiristAdapter } from "@/Router/adapters/MemoiristAdapter";
 export type { RouterAdapterInterface } from "@/Router/adapters/RouterAdapterInterface";
 export type { RouterReturnData } from "@/Router/types/RouterReturnData";
 export type { RouterRouteData } from "@/Router/types/RouterRouteData";
+
+export const $prefixStore = new GlobalPrefixStore();
+export const $routerStore = new GlobalRouterStore();
 
 export type * from "./types.d.ts";
 export * from "@/C";
